5780ee2ce49a0e26692e02b730ad4e5eeea46a29
HRB213 <i2686262@cc.kyoto-su.ac.jp>
2026-06-27 04:52:24 +0900

    chore: update distribution script for herobi

--- .github/scripts/make_distribution_files.sh
+++ .github/scripts/make_distribution_files.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
+
 TAG=$1
-PRODUCT_NAME=lis
+PRODUCT_NAME=herobi
 RELEASE=$PRODUCT_NAME-$TAG-arm64-darwin
+
 cargo build --release
+
 mkdir -p dist/$RELEASE
-cp LICENSE README.md target/release/lis dist/$RELEASE
+
+cp LICENSE README.md target/release/herobi dist/$RELEASE
+
 tar cvfz dist/$RELEASE.tar.gz -C dist $RELEASE
